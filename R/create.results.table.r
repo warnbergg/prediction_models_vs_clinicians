26a70f5f489fbffec551abad6bd01a6142194ab9
janejkanske <ludviggerdin@gmail.com>
2018-07-06 00:08:06 +0200

    Removed most of lines to make more generic

--- R/create.results.table.r
+++ R/create.results.table.r
@@ -1,53 +1,13 @@
 #' Create results table function
 #'
-#' This function creates the results table, i.e. AUC and NRI for each model
-#' @param analysis_lst List. Estimates of models. No default.
+#' This function creates the results table, i.e. tabulates condfidence intervals and sample estimates for models.
+#' @param analysis_objs List of data frames. Each containing ci around estimates by models. No default.
 #' @export
-create.results.table <- function(analysis_lst){
-    ## Merge reclassifciation columns to one ci column
-    analysis_lst <- lapply(analysis_lst, function(analysis_type){
-        lapply(analysis_type, function (model_df) {
-            ## Merge columns to paranthesised ci
-            model_df$ci <- with(model_df, paste(sample_estimate,
-                                                paste0("(",
-                                                       paste(lb, "to", ub),
-                                                       ")")))
-            ## Remove original columns
-            model_df[, c("lb", "ub", "sample_estimate")]<- NULL
+create.results.table <- function(analysis_objs){
+    ## Error handling
+    if (!(all(unlist(lapply(analysis_objs, is.data.frame))))) stop ("Argument must be list of data frames")
+    ## Merge analysis_objs list to single data.frame
+    merged <- do.call(cbind, analysis_objs)
 
-            return(model_df)
-        })
-    })
-    ## Tabulate reclassification
-    reclassification_table <- as.data.frame(analysis_lst$reclassification)
-    colnames(reclassification_table) <- names(analysis_lst$reclassification)
-    ## Tabulate auc
-    auc_table <- t(do.call(rbind, analysis_lst$AUROCC))
-    ## Fill reclassification table for analysis table, and
-    ## auc table according to vice versa
-    filled_reclass <- cbind(reclassification_table,
-                            matrix(rep(rep("", nrow(reclassification_table)),
-                                       ncol(reclassification_table)),
-                                   ncol = ncol(reclassification_table)))
-    ## Set full colnames
-    colnames(filled_reclass) <- colnames(auc_table)
-    ## Merge reclass and auc tables
-    analysis_table <- rbind(AUROCC = auc_table, filled_reclass)
-    ## xtable the analysis table
-    the_table <- xtable::print.xtable(xtable::xtable(analysis_table,
-                                                     caption = "\\bf Table of estimates.",
-                                                     label = "tab:analysis_table"),
-                                      type = "latex",
-                                      booktabs = TRUE,
-                                      table.placement = "!ht",
-                                      include.rownames = TRUE,
-                                      include.colnames = TRUE,
-                                      caption.placement = "top",
-                                      print.results = FALSE)
-    ## Format, i.e. add tabular and adjustbox environments
-    the_table <- formatting.xtable(the_table)
-    ## Save table
-    write(the_table, "analysis_table.tex")
-
-    return (analysis_table)
+    return (merged)
 }
