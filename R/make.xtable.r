023c5a61f82ad4dc90f315c1790284541c64ef0d
janejkanske <ludviggerdin@gmail.com>
2018-07-07 17:20:02 +0200

    Removed default file name to saving

--- R/make.xtable.r
+++ R/make.xtable.r
@@ -1,24 +1,27 @@
-#' Title of function
+#' Make xtable function
 #'
-#' Description of function.
-#' @param x Integer. Short description. Default/No default.
-#' @param y String. Short description. Default/No default.
-#' @param z Logical. Short description. Defaults to TRUE.
+#' This function inputs table data and saves to .tex file as xtable.
+#' @param table_data. Data frame. No default.
+#' @param caption String. Caption for table. Defaults to "Table of estimates".
+#' @param label String. Latex label. Defaults to "analysis_table".
+#' @param file_name String. File name. No default.
 #' @export
-make.xtable <- function(table_data, caption, label, ){
+make.xtable <- function(table_data,
+                        caption = "Table of Estimates.", label = "analysis_table",
+                        include_rownames = TRUE, file_name){
     ## xtable the analysis table
-    the_table <- xtable::print.xtable(xtable::xtable(table_data
-                                                     caption = "\\bf Table of estimates.",
-                                                     label = "tab:analysis_table"),
+    the_table <- xtable::print.xtable(xtable::xtable(table_data,
+                                                     caption = paste("\bf", caption),
+                                                     label = paste0("tab:", label)),
                                       type = "latex",
                                       booktabs = TRUE,
                                       table.placement = "!ht",
-                                      include.rownames = TRUE,
+                                      include.rownames = include_rownames,
                                       include.colnames = TRUE,
                                       caption.placement = "top",
                                       print.results = FALSE)
     ## Format, i.e. add tabular and adjustbox environments
     the_table <- formatting.xtable(the_table)
     ## Save table
-    write(the_table, "analysis_table.tex")
+    write(the_table, file = file_name)
     }
