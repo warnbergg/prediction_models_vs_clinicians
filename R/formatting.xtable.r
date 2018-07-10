e3d58ab2b34e1670858131b3dc0234e6cd88ee10
janejkanske <ludviggerdin@gmail.com>
2018-07-10 07:43:50 +0200

    Minor changes

--- R/formatting.xtable.r
+++ R/formatting.xtable.r
@@ -3,18 +3,19 @@
 #' This function takes a xtable print and formats.
 #' @param the_xtable As xtable print. No default.
 #' @export
-formatting.xtable <- function(the_xtable){
+formatting.xtable <- function(the_xtable,
+                              minipage = TRUE){
     ## Add begin tabular and adjustbox environments
     the_xtable <- sub("\\begin{tabular}",
-                           paste0("\\begin{adjustbox}{max width=\\linewidth} \n",
+                           paste0("\\begin{adjustbox}{max width = \\linewidth} \n",
                                   "\\begin{tabular} \n"),
                            the_xtable,
-                           fixed = TRUE)
+                      fixed = TRUE)
     ## Add end to tabular and adjustbox environments
     the_xtable <- sub("\\end{tabular}",
                            paste0("\\end{tabular} \n",
                                   "\\end{adjustbox}"),
                            the_xtable,
-                           fixed = TRUE)
+                      fixed = TRUE)
     return (the_xtable)
     }
