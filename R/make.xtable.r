e586d68158163f9ff86ad986a54c5819ac68c95c
janejkanske <ludviggerdin@gmail.com>
2018-07-10 07:43:31 +0200

    Added sanitize options

--- R/make.xtable.r
+++ R/make.xtable.r
@@ -2,13 +2,15 @@
 #'
 #' This function inputs table data and saves to .tex file as xtable.
 #' @param table_data. Data frame. No default.
+#' @param san_row Function. Identity function for sanitize.rownames.function. Defaults to NULL.
+#' @param san_col Function. Identity function for sanitize.colnames.function. Defaults to NULL.
 #' @param caption String. Caption for table. Defaults to "Table of estimates".
 #' @param label String. Latex label. Defaults to "analysis_table".
 #' @param file_name String. File name. No default.
 #' @export
-make.xtable <- function(table_data,
+make.xtable <- function(table_data, file_name, san_row = NULL, san_col = NULL,
                         caption = "Table of Estimates.", label = "analysis_table",
-                        include_rownames = TRUE, file_name){
+                        include_rownames = TRUE){
     ## xtable the analysis table
     the_table <- xtable::print.xtable(xtable::xtable(table_data,
                                                      caption = paste("\\bf", caption),
@@ -18,6 +20,8 @@ make.xtable <- function(table_data,
                                       table.placement = "!ht",
                                       include.rownames = include_rownames,
                                       include.colnames = TRUE,
+                                      sanitize.rownames.function = san_row,
+                                      sanitize.colnames.function = san_col,
                                       caption.placement = "top",
                                       print.results = FALSE)
     ## Format, i.e. add tabular and adjustbox environments
