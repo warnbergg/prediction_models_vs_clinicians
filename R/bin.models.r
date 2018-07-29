b3b97848bacec863265ef22ea085e4a5293cb3f9
janejkanske <ludviggerdin@gmail.com>
2018-07-29 23:49:48 +0200

    Minor modifications

--- R/bin.models.r
+++ R/bin.models.r
@@ -2,13 +2,13 @@
 #'
 #' This function bins model predictions using SupaLarnas gridsearch.breaks function.
 #' @param predictions A numeric vector of predicted probabilites. No default.
-#' @param outomes A numeric vector of 0 and 1. No default.
-#' @param grid All values to be combnd in gridsearch.breaks, i.e. used to find optimal cutpoints. No default.
+#' @param outcomes A numeric vector of 0 and 1. No default.
 #' @param n_cores Number of cores to be used in parallel gridsearch. Passed to SupaLarna::gridsearch.breaks. As integer. Defaults to 2 (in gridsearch.breaks)
+#' @param grid All values to be combnd in gridsearch.breaks, i.e. used to find optimal cutpoints. No default.
 #' @param return_cps Logical. Function returns model cut_points if TRUE. Defaults to TRUE.
 #' @param gridsearch_parallel Logical. If TRUE the gridsearch is performed in parallel. Defaults to FALSE.
 #' @param is_sample Logical. If TRUE, only a tenth of possible cut points is searched. Defaults to TRUE.
-#' @param maximise Logical. If TRUE, grid search maximizes performance metric. Passed to SupaLarna::gridsearch.breaks. Defaults to TRUE.
+#' @param maximise Logical. If TRUE, grid search maximises performance metric. Passed to SupaLarna::gridsearch.breaks. Defaults to TRUE.
 #' @export
 bin.models <- function(
                        predictions,
