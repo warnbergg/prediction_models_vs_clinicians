16b83da4b4f996b9f3d57408597c83bc89a7efdd
Ludvig Wärnberg Gerdin <ludviggerdin@gmail.com>
2018-10-06 08:02:51 +0200

    Adjusting for table footnotes with threeparttable

--- R/formatting.xtable.r
+++ R/formatting.xtable.r
@@ -2,19 +2,30 @@
 #'
 #' This function takes a xtable print and formats.
 #' @param the_xtable As xtable print. No default.
+#' @param table_notes String. The string to be put under the table as note. Not dynamic, only one note allowed. Defaults to NULL.
 #' @export
 formatting.xtable <- function(the_xtable,
-                              minipage = TRUE){
+                              table_notes = NULL){
+    ## Error handling
+    if (!is.null(table_notes) && !(length(table_notes) == 1)) stop("Table notes should be of length 1.")
     ## Add begin tabular and adjustbox environments
     the_xtable <- sub("\\begin{tabular}",
-                           paste0("\\begin{adjustbox}{max width = \\linewidth} \n",
-                                  "\\begin{tabular} \n"),
+                      paste0("\\begin{adjustbox}{max width = \\linewidth} \n",
+                             "\\begin{threeparttable} \n",
+                             "\\begin{tabular} \n"),
                            the_xtable,
                       fixed = TRUE)
+    ## Define table notes in tablenotes call
+    if (!is.null(table_notes)) table_notes <- paste0("\\begin{tablenotes} \\footnotesize \n",
+                                                     sprintf("\\item[*] %s \n", table_notes),
+                                                     "\\end{tablenotes} \n")
     ## Add end to tabular and adjustbox environments
     the_xtable <- sub("\\end{tabular}",
-                           paste0("\\end{tabular} \n",
-                                  "\\end{adjustbox}"),
+                      paste0("\\addlinespace \n",
+                             table_notes,
+                             "\\end{tabular} \n",
+                             "\\end{threeparttable} \n",
+                             "\\end{adjustbox}"),
                            the_xtable,
                       fixed = TRUE)
     return (the_xtable)
